f28963feab8112de063b8925d839a3cec2bdc425
Jakub Dolata <jdolata5@gmail.com>
2023-07-16 17:13:41 +0200

    innit commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,10 @@
  */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: ``,
+    title: `Cortado`,
+    description: `Najlepsza kawa dla Twojej firmy!`,
+    author: `@springStudio`,
   },
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
