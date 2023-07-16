576325f105c8052877a55eb77699a38085371720
Jakub Dolata <jdolata5@gmail.com>
2023-07-16 19:51:18 +0200

    add typhography and variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,23 +2,24 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    siteUrl: `https://kawacortado.pl`,
-    title: `Cortado`,
-    description: `Najlepsza kawa dla Twojej firmy!`,
-    author: `@springStudio`,
-  },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        siteUrl: `https://kawacortado.pl`,
+        title: `Cortado`,
+        description: `Najlepsza kawa dla Twojej firmy!`,
+        author: `@springStudio`,
     },
-    __key: "images"
-  }]
+    plugins: ["gatsby-plugin-sass", "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap",
+        "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                "icon": "src/images/icon.png"
+            }
+        }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
+        }]
 };
---
+++ src/theme/Theme.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Theme = ({children}) => {
+    return (
+        <main>
+            {children}
+        </main>
+    );
+};
+
+export default Theme;
