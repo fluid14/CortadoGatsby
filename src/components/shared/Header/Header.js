502b68b1bf56b2d89426e62528d9bd407d9a7889
Jakub Dolata <jdolata5@gmail.com>
2023-07-18 17:20:23 +0200

    add desktop header styles

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,37 +1,59 @@
 import React from "react";
 import * as styles from './Header.module.scss';
 import Button from "../Button/Button";
+import {StaticImage} from "gatsby-plugin-image"
+import {Link} from "gatsby"
 
 const Header = () => {
     return (
-        <header className={styles.header}>
-            <div className={styles.logoWrap}>
-                <p className={styles.logo}>
-                    Cortado
-                </p>
-                <p className={styles.contact}>
-                    Zapytaj o produkt
-                    <a className={styles.contactLink} href='mailto: info@cortado.pl'>info@cortado.pl</a>
-                </p>
-            </div>
-
-            <div className={styles.actionWrap}>
-                <img src="" alt=""/>
-                <Button size='small'>
-                    Chcę subskrybować
-                </Button>
-
-                <div className={styles.loginWrap}>
-                    <Button size='medium' text secondary>
-                        Logowanie
-                    </Button>
-                    <span> / </span>
-                    <Button size='medium' text>
-                        Rejestracja
-                    </Button>
+        <div className={styles.headerWrap}>
+            <header className={styles.header}>
+                <div className={styles.logoWrap}>
+                    <p className={styles.logo}>
+                        Cortado
+                    </p>
+                    <p className={styles.contact}>
+                        Zapytaj o produkt
+                        <a className={styles.contactLink} href='mailto: info@cortado.pl'>info@cortado.pl</a>
+                    </p>
                 </div>
-            </div>
-        </header>
+
+                <div className={styles.actionWrap}>
+                    <img src="" alt=""/>
+                    <Button size='small'>
+                        Chcę subskrybować
+                    </Button>
+
+                    <div className={styles.loginWrap}>
+                        <StaticImage className={styles.loginImage} src='../../../images/icons/person.svg' alt='user'/>
+                        <Button className={styles.login} size='medium' text secondary>
+                            Logowanie
+                        </Button>
+                        <span> / </span>
+                        <Button className={styles.register} size='medium' text>
+                            Rejestracja
+                        </Button>
+                    </div>
+                </div>
+            </header>
+
+            <nav className={styles.nav}>
+                <ul className={styles.navList}>
+                    <li className={styles.navItem}>
+                        <Link className={styles.navLink} activeClassName={styles.active} to="/">Start</Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link className={styles.navLink} activeClassName={styles.active} to="/blog">Jak to działa</Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link className={styles.navLink} activeClassName={styles.active} to="/blog">Nasze kawy</Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link className={styles.navLink} activeClassName={styles.active} to="/blog">Kontakt</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 };
 
