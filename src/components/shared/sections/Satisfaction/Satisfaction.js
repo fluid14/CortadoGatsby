9437cf50cfb3d4327cd88ec38f81a36b7f752522
Jakub Dolata <jdolata5@gmail.com>
2023-07-22 18:36:21 +0200

    add satisfaction desktop styles

--- src/components/shared/sections/Satisfaction/Satisfaction.js
+++ src/components/shared/sections/Satisfaction/Satisfaction.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import * as styles from './Satisfaction.module.scss';
 import cs from 'classnames';
+import leafImage from '../../../../images/leaf.svg';
+import sunImage from '../../../../images/sun.svg';
 
 const Satisfaction = () => {
   return (
     <section className={cs('section', styles.satisfactionWrap)}>
-      <h3 className={styles.text}>W bonusie: zadowolenie współpracowników gwarantowane!</h3>
+      <h3 className={styles.text}>
+        <span className={styles.title}>W bonusie:</span> zadowolenie współpracowników gwarantowane!
+      </h3>
+
+      <img className={styles.imageSun} src={sunImage} alt="Słońce" />
+      <img className={styles.imageLeaf} src={leafImage} alt="Liść" />
     </section>
   );
 };
