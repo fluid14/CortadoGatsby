3c8553aae49a2c7340a65cb85e15edd50bb6f7ad
Jakub Dolata <jdolata5@gmail.com>
2023-08-21 17:02:19 +0200

    try to fix loading statuc query

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -2,84 +2,112 @@ import React from 'react';
 import * as styles from './Footer.module.scss';
 import Logo from '../Logo/Logo';
 import cs from 'classnames';
-import { graphql, StaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import { AnchorLink } from 'gatsby-plugin-anchor-links';
 
+// const Footer = () => {
+//   return (
+//     <StaticQuery
+//       query={graphql`
+//         query FooterQuery {
+//           strapiFooter {
+//             address
+//             addressTitle
+//             city
+//             email
+//             logoText
+//             phone
+//             postalCode
+//             sectionInfo {
+//               sectionId
+//             }
+//             navigation {
+//               title
+//               url
+//               id
+//             }
+//           }
+//         }
+//       `}
+//       render={(footer) => <FooterComponent data={footer} />}
+//     />
+//   );
+// };
+
 const Footer = () => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query FooterQuery {
-          strapiFooter {
-            address
-            addressTitle
-            city
-            email
-            logoText
-            phone
-            postalCode
-            sectionInfo {
-              sectionId
-            }
-            navigation {
-              title
-              url
-              id
-            }
-          }
+  const data = useStaticQuery(graphql`
+    query FooterQuery {
+      strapiFooter {
+        address
+        addressTitle
+        city
+        email
+        logoText
+        phone
+        postalCode
+        sectionInfo {
+          sectionId
         }
-      `}
-      render={(footer) => <FooterComponent data={footer} />}
-    />
-  );
-};
+        navigation {
+          title
+          url
+          id
+        }
+      }
+    }
+  `);
 
-const FooterComponent = ({ data: { strapiFooter } }) => {
-  console.log(strapiFooter);
   const {
-    address,
-    addressTitle,
-    city,
-    email,
-    logoText,
-    phone,
-    postalCode,
-    sectionInfo: { sectionId },
-    navigation,
-  } = strapiFooter;
+    strapiFooter: {
+      address,
+      addressTitle,
+      city,
+      email,
+      logoText,
+      phone,
+      postalCode,
+      sectionInfo: { sectionId },
+      navigation,
+    },
+  } = data;
+
   return (
-    <footer className={cs('section fullWidth small', styles.footer)}>
-      <Logo className={styles.logo}>{logoText}</Logo>
+    <>
+      {data && (
+        <footer className={cs('section fullWidth small', styles.footer)}>
+          <Logo className={styles.logo}>{logoText}</Logo>
 
-      <div className={styles.content}>
-        <ul className={styles.menu}>
-          {navigation.map(({ id, title, url }) => (
-            <li className={styles.menuItem} key={id}>
-              <AnchorLink to={url} className={styles.menuLink}>
-                {title}
-              </AnchorLink>
-            </li>
-          ))}
-        </ul>
+          <div className={styles.content}>
+            <ul className={styles.menu}>
+              {navigation.map(({ id, title, url }) => (
+                <li className={styles.menuItem} key={id}>
+                  <AnchorLink to={url} className={styles.menuLink}>
+                    {title}
+                  </AnchorLink>
+                </li>
+              ))}
+            </ul>
 
-        <div className={styles.contact} id={sectionId}>
-          <p className={styles.contactCompany}>{addressTitle}</p>
-          <p className={styles.contactText}>{address}</p>
-          <p className={styles.contactText}>
-            {postalCode} {city}
-          </p>
-        </div>
+            <div className={styles.contact} id={sectionId}>
+              <p className={styles.contactCompany}>{addressTitle}</p>
+              <p className={styles.contactText}>{address}</p>
+              <p className={styles.contactText}>
+                {postalCode} {city}
+              </p>
+            </div>
 
-        <div className={styles.contactLinks}>
-          <a className={styles.contactText} href={`tel: ${phone}`}>
-            {phone}
-          </a>
-          <a className={styles.contactText} href={`mailto: ${email}`}>
-            {email}
-          </a>
-        </div>
-      </div>
-    </footer>
+            <div className={styles.contactLinks}>
+              <a className={styles.contactText} href={`tel: ${phone}`}>
+                {phone}
+              </a>
+              <a className={styles.contactText} href={`mailto: ${email}`}>
+                {email}
+              </a>
+            </div>
+          </div>
+        </footer>
+      )}
+    </>
   );
 };
 
